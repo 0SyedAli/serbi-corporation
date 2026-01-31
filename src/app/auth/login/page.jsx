569fa50f37bb776615eb659f2c318838509ceadd
0SyedAli <sa8449006@gmail.com>
2026-01-31 05:39:20 +0500

    Update page.jsx

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -29,7 +29,7 @@ export default function LoginPage() {
           <input
             type="email"
             className="form-control auth-input"
-            placeholder="name@school.edu"
+            placeholder="name@example.edu"
             {...register("email")}
           />
         </div>
